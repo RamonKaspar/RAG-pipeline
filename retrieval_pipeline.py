f2f5c188b58a33b268a8c3426a725582f5a23513
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-07-14 14:39:06 +0200

    Rename num_snippets to top_k to follow naming conventions

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -19,12 +19,12 @@ class RetrievalPipeline:
         self.embedded_data = self.load_embedded_data(subject)   # pandas DataFrame containing the embedded data
         
 
-    def retrieve(self, user_query: str, num_snippets: int, threshold: float) -> List[str]:
+    def retrieve(self, user_query: str, top_k: int, threshold: float) -> List[str]:
         """Runs the pipeline for the RAG model. Returns the response retrieved by the model.
 
         Args:
             user_query (str): The query for which we search for relevant information.
-            num_snippets (int): The number of snippets to retrieve.
+            top_k (int): The number of snippets to retrieve.
             threshold (float): The threshold for the similarity search in the retrieval.
 
         Returns:
@@ -35,11 +35,11 @@ class RetrievalPipeline:
         retrieved_docs = self.retrieve_documents(user_query)
         
         # Step 2: Select most relevant Documents
-        selected_snippets = self.select_documents(retrieved_docs, num_snippets, threshold)
+        selected_snippets = self.select_documents(retrieved_docs, top_k, threshold)
         
         # TODO: Whatever remaining steps are needed
         
-        assert len(selected_snippets) <= num_snippets   # Ensure that the number of snippets is less than or equal to the number of snippets requested
+        assert len(selected_snippets) <= top_k   # Ensure that the number of snippets is less than or equal to the number of snippets requested
         return selected_snippets
     
     def retrieve_documents(self, user_query: str) -> List[str]:
